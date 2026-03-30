fa3ec5d5ea8356ebab4a6e40adc591a3be4d3677
chaitanaya pandit <chaitanayapandit@gmail.com>
2026-03-30 15:05:36 +0530

    typescript version of todoslist

---
+++ day2/bank_account.ts
@@ -0,0 +1,40 @@
+class todos {
+    constructor(public id: number, public title: string, public description: string, public status: string) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status="pending"
+    }
+
+    mark_complete():void{
+        this.status="completed"
+    }
+    mark_not_complete():void{
+        this.status="pending"
+    }
+    update_title(new_title:string):void{
+        this.title= new_title
+    }
+    update_description(new_desc:string):void{
+        this.description= new_desc
+    }
+    get_todo_details(){
+        return {
+            id: this.id,
+            title: this.title,
+            description: this.description,
+            status: this.status
+        }
+    }
+}
+class todolists {
+    constructor(public name: string, public todos:todos[]){
+        this.name=name;
+        this.todos=todos;
+    }
+
+    add_todo(todo:todos):void{
+        const added_todo = new todos(todo.id, todo.title, todo.description, todo.status)
+        this.todos.push(added_todo)
+    }
+}
